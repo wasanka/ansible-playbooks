0f53beeb719975423e2ae18c15cf1af158986d23
wasanka <wasanka2000@gmail.com>
2024-09-02 16:22:06 -0400

    Create process-info-mqtt-pub.sh

---
+++ resources/process-info-mqtt-pub.sh
@@ -0,0 +1,29 @@
+echo "<payload>" > message.txt
+echo "<hostname>`hostname`</hostname>" >> message.txt
+echo "<type>MEMORY_INFO</type>" >> message.txt
+echo "<message>" >> message.txt
+cat /proc/meminfo >> message.txt
+echo "</message>" >> message.txt
+echo "</payload>" >> message.txt
+mosquitto_pub -L mqtt://semaphore@192.168.1.171/testTopic -f message.txt
+rm message.txt
+
+echo "<payload>" > message.txt
+echo "<hostname>`hostname`</hostname>" >> message.txt
+echo "<type>CPU_INFO</type>" >> message.txt
+echo "<message>" >> message.txt
+mpstat -P ALL >> message.txt
+echo "</message>" >> message.txt
+echo "</payload>" >> message.txt
+mosquitto_pub -L mqtt://semaphore@192.168.1.171/testTopic -f message.txt
+rm message.txt
+
+echo "<payload>" > message.txt
+echo "<hostname>`hostname`</hostname>" >> message.txt
+echo "<type>PROCESS_INFO</type>" >> message.txt
+echo "<message>" >> message.txt
+ps aux >> message.txt
+echo "</message>" >> message.txt
+echo "</payload>" >> message.txt
+mosquitto_pub -L mqtt://semaphore@192.168.1.171/testTopic -f message.txt
+rm message.txt
