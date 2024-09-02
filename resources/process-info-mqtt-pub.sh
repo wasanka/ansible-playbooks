645139bdcf0fbd36dd41838a28b029f3fe308dcf
wasanka <wasanka2000@gmail.com>
2024-09-02 17:49:17 -0400

    Update process-info-mqtt-pub.sh

--- resources/process-info-mqtt-pub.sh
+++ resources/process-info-mqtt-pub.sh
@@ -1,9 +1,9 @@
 echo "<payload>" > message.txt
 echo "<hostname>`hostname`</hostname>" >> message.txt
 echo "<type>MEMORY_INFO</type>" >> message.txt
-echo "<message>" >> message.txt
+echo "<message><![CDATA[" >> message.txt
 cat /proc/meminfo >> message.txt
-echo "</message>" >> message.txt
+echo "]]></message>" >> message.txt
 echo "</payload>" >> message.txt
 mosquitto_pub -L mqtt://semaphore@192.168.1.171/testTopic -f message.txt
 rm message.txt
@@ -11,9 +11,9 @@ rm message.txt
 echo "<payload>" > message.txt
 echo "<hostname>`hostname`</hostname>" >> message.txt
 echo "<type>CPU_INFO</type>" >> message.txt
-echo "<message>" >> message.txt
+echo "<message><![CDATA[" >> message.txt
 mpstat -P ALL >> message.txt
-echo "</message>" >> message.txt
+echo "]]></message>" >> message.txt
 echo "</payload>" >> message.txt
 mosquitto_pub -L mqtt://semaphore@192.168.1.171/testTopic -f message.txt
 rm message.txt
@@ -21,9 +21,9 @@ rm message.txt
 echo "<payload>" > message.txt
 echo "<hostname>`hostname`</hostname>" >> message.txt
 echo "<type>PROCESS_INFO</type>" >> message.txt
-echo "<message>" >> message.txt
+echo "<message><![CDATA[" >> message.txt
 ps aux >> message.txt
-echo "</message>" >> message.txt
+echo "]]></message>" >> message.txt
 echo "</payload>" >> message.txt
 mosquitto_pub -L mqtt://semaphore@192.168.1.171/testTopic -f message.txt
 rm message.txt
